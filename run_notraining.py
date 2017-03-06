716f00a9e8d2c72082223368a1f20996d987b577
hadyelsahar <hadyelsahar@gmail.com>
2017-03-06 14:59:36 +0100

    adding validation learning PARAMS

--- run_notraining.py
+++ run_notraining.py
@@ -13,6 +13,7 @@ from utils.vectorizers.attention_vectorizer import DepAttentionVectorizer
 import numpy as np
 from nltk.tokenize import word_tokenize
 import argparse
+from itertools import product
 
 class Run:
     def __init__(self, df, glovefile):
@@ -55,17 +56,17 @@ class Run:
         #####################
         # Sentence Features #
         #####################
-        # idf = TfidfVectorizer(binary=True)
-        # s_idf = idf.fit_transform(self.data['sentence'].values).toarray()
+        idf = TfidfVectorizer(binary=True)
+        s_idf = idf.fit_transform(self.data['sentence'].values).toarray()
 
         # Sentence With Entities Replaced by Types
-        # ss = self.data.apply(lambda x: x['sentence'].replace(x['sub'], x['type'].split('-')[0]).replace(x['obj'], x['type'].split('-')[1]), axis=1)
-        # countvectorizer = CountVectorizer()
-        # ss_count = countvectorizer.fit_transform(ss).toarray()
-        # tfidf = TfidfVectorizer()
-        # ss_tfidf = tfidf.fit_transform(ss).toarray()
-        # idf = TfidfVectorizer(binary=True)
-        # ss_idf= idf.fit_transform(ss).toarray()
+        ss = self.data.apply(lambda x: x['sentence'].replace(x['sub'], x['type'].split('-')[0]).replace(x['obj'], x['type'].split('-')[1]), axis=1)
+        countvectorizer = CountVectorizer()
+        ss_count = countvectorizer.fit_transform(ss).toarray()
+        tfidf = TfidfVectorizer()
+        ss_tfidf = tfidf.fit_transform(ss).toarray()
+        idf = TfidfVectorizer(binary=True)
+        ss_idf= idf.fit_transform(ss).toarray()
 
         # Sentence Glove Word2Vec
         glove_vectorizer = GloveVectorizer(self.glovefile)
@@ -98,49 +99,84 @@ class Run:
         ###### Adding Features ####
         self.features += [
             ### sentences ###
-            # {'name': "sentence_idf", 'feature': s_idf, 'PCA': True, 'PCA_size': 10},
-            # {'name': "sentence_prep_count", 'feature': ss_count, 'PCA': True, 'PCA_size': 10},
-            # {'name': "sentence_prep_tfidf", 'feature': ss_tfidf, 'PCA': True, 'PCA_size': 10},
-            # {'name': "sentence_prep_idf", 'feature': ss_idf, 'PCA': True, 'PCA_size': 10},
-            # {'name': "word2vec_sum", 'feature': w2v, 'PCA': True, 'PCA_size': 20},
-            {'name': "dependency_attention_w2v", 'feature': depattention_ww2v, 'PCA': True, 'PCA_size': 10},
+            {'name': "sentence_idf", 'feature': s_idf, 'PCA': True, 'PCA_size': 10, 'PCA_test_range': range(0, 100, 20)},
+            {'name': "sentence_prep_count", 'feature': ss_count, 'PCA': True, 'PCA_size': 10, 'PCA_test_range': range(0, 100, 20)},
+            {'name': "sentence_prep_tfidf", 'feature': ss_tfidf, 'PCA': True, 'PCA_size': 10, 'PCA_test_range': range(0, 100, 20)},
+            {'name': "sentence_prep_idf", 'feature': ss_idf, 'PCA': True, 'PCA_size': 10, 'PCA_test_range': range(0, 100, 20)},
+            {'name': "word2vec_sum", 'feature': w2v, 'PCA': True, 'PCA_size': 20, 'PCA_test_range': range(0, 100, 20)},
+            {'name': "dependency_attention_w2v", 'feature': depattention_ww2v, 'PCA': True, 'PCA_size': 10, 'PCA_test_range': range(0, 100, 20)},
             ### types ###
-            {'name': "type_sub", 'feature': type_sub, 'PCA': True, 'PCA_size': 2},
-            {'name': "type_obj", 'feature': type_obj, 'PCA': True, 'PCA_size': 2},
-            # {'name': "type_sub_obj", 'feature': type_sub_obj, 'PCA': True, 'PCA_size': 4},
+            {'name': "type_sub", 'feature': type_sub, 'PCA': True, 'PCA_size': 2, 'PCA_test_range': range(0, 5, 1)},
+            {'name': "type_obj", 'feature': type_obj, 'PCA': True, 'PCA_size': 2, 'PCA_test_range': range(0, 5, 1)},
+            {'name': "type_sub_obj", 'feature': type_sub_obj, 'PCA': True, 'PCA_size': 4, 'PCA_test_range': range(0, 5, 1)},
             # KB Types ##
-            {'name': "kbtype_sub", 'feature': kbtype_sub, 'PCA': True, 'PCA_size': 5},
-            {'name': "kbtype_obj", 'feature': kbtype_obj, 'PCA': True, 'PCA_size': 5},
+            {'name': "kbtype_sub", 'feature': kbtype_sub, 'PCA': True, 'PCA_size': 5, 'PCA_test_range': range(0, 10, 2)},
+            {'name': "kbtype_obj", 'feature': kbtype_obj, 'PCA': True, 'PCA_size': 5, 'PCA_test_range': range(0, 10, 2)},
         ]
 
-        tmp = []
-        for f in self.features:
-            if f['PCA']:
-                pca = PCA(n_components=f['PCA_size'])
-                tmp.append(pca.fit_transform(f['feature']))
-            else:
-                tmp.append(f['feature'])
+        # tmp = []
+        # for f in self.features:
+        #     if f['PCA']:
+        #         pca = PCA(n_components=f['PCA_size'])
+        #         tmp.append(pca.fit_transform(f['feature']))
+        #     else:
+        #         tmp.append(f['feature'])
+        #
+        # X = np.hstack(tmp)
+        # # print "returning dense"
+        # self.vectorizeddata = X
 
-        X = np.hstack(tmp)
-        # print "returning dense"
-        self.vectorizeddata = X
+        self.tune_clusters(self.features)
 
         return self.vectorizeddata
 
-    def cluster(self, x, clustering=None, n_clusters=6):
+    def cluster(self, x, clustering=None, n_clusters=100, labels=None):
+
+        if labels is None:
+            labels = self.labels
 
         # print "Starting Kmeans clustering.."
         if clustering is None:
             clustering = KMeans(n_clusters=n_clusters)
 
-        predictions_minibatch = clustering.fit_predict(x)
+        pred = clustering.fit_predict(x)
         # print "done Kmeans clustering.."
-        self.clusters = predictions_minibatch
+        self.clusters = pred
 
-        e = ClusterEvaluation(self.labels, predictions_minibatch)
+        e = ClusterEvaluation(labels, pred)
         m = e.printEvaluation()
 
         return m
+
+    def tune_clusters(self, features, labels=None, n_clusters=100):
+
+
+        maxf1 = 0
+        ranges = [f['PCA_test_range'] for f in features if 'PCA_test_range' in f]
+        combinations = product(*ranges)
+
+        for c in combinations:
+            tmp = []
+            for n, f in enumerate(features):
+                if c[n] == 0:
+                    continue
+                elif f['PCA']:
+                    pca = PCA(n_components=c[n])
+                    tmp.append(pca.fit_transform(f['feature']))
+                else:
+                    tmp.append(f['feature'])
+
+            if len(tmp) == 0:
+                continue
+
+            X = np.hstack(tmp)
+            f1 = self.cluster(X, n_clusters=100)['Elementwise B3 F1']
+
+            if f1 > maxf1:
+                maxf1 = f1
+                print "Max F1 score : %s" % maxf1
+                print "\n".join(["%s:%s" % (i['name'], c[j]) for j, i in enumerate(features)])
+                self.vectorizeddata = X
 
 
 if __name__ == "__main__":
